4ee6527aa345173d610c525ba7735515e76348e3
Carlos <1310374@utp.edu.pe>
2019-04-14 16:12:26 -0500

    actualizando escritorios

--- public/js/socket.escritorio.js
+++ public/js/socket.escritorio.js
@@ -9,22 +9,17 @@ if (!searchParams.has("escritorio")) {
 
 var escritorio = searchParams.get('escritorio');
 var label = $('small');
-console.log(escritorio);
+//console.log(escritorio);
 
-if (escritorio > 0 && escritorio < 5) {
-    $('h1').text('Escritorio ' + escritorio);
+$('h1').text('Escritorio ' + escritorio);
 
-    $('button').on('click', function () {
-        socket.emit('atenderTicket', { escritorio: escritorio }, function (resp) {
-            if (resp === 'No hay más tickets') {
-                //alert(resp);
-                label.text(resp);
-                return;
-            }
-            label.text('Ticket ' + resp.numero);
-        });
+$('button').on('click', function () {
+    socket.emit('atenderTicket', { escritorio: escritorio }, function (resp) {
+        if (resp === 'No hay más tickets') {
+            //alert(resp);
+            label.text(resp);
+            return;
+        }
+        label.text('Ticket ' + resp.numero);
     });
-} else {
-    alert('Debe ingresar un escritorio entre 1 a 4');
-    window.location = "index.html";
-}
+});
